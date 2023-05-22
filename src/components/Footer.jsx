9623f309562732c26e8b489976b6e9344caa8253
taleshrocha <taleshrochaz@gmail.com>
2023-05-22 15:51:56 -0300

    NavBar trasitions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,35 @@
-import styles from "../styles/components/Footer.module.css"
+import styles from "../styles/components/Footer.module.css";
+import { 
+  FaFacebookF as FacebookLogoIcon ,
+  FaInstagram as InstagramLogoIcon
+} from 'react-icons/fa'
+import Link from "next/link";
 
-export default  function Footer() {
+export default function Footer() {
   return (
-  <footer className={styles.footer}>
-      <p>Facebook</p>
-      <p>Instagram</p>
-  </footer>
-  )
+    <footer className={styles.footer}>
+      <LinkButton
+        Icon={FacebookLogoIcon}
+        href={"https://www.google.com"}
+      />
+
+      <LinkButton
+        Icon={InstagramLogoIcon}
+        href={"https://www.google.com"}
+      />
+    </footer>
+  );
+}
+
+function LinkButton({ Icon, href }) {
+  return (
+    <Link
+      className={styles.linkButton}
+      href={href}
+    >
+      <Icon 
+        size={30}
+      />
+    </Link>
+  );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,24 +2,47 @@
 import styles from "../styles/components/NavBar.module.css";
 import {
   AiOutlineMenu as MenuIcon,
-AiFillCloseCircle as CloseIcon
+  AiFillCloseCircle as CloseIcon
 } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function NavBar() {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const [isScrollDown, setIsScrollDown] = useState(false);
+
+  useEffect(() => {
+    let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    const handleScroll = () => {
+      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentScrollTop > lastScrollTop) {
+        setIsScrollDown(true);
+      } 
+      else setIsScrollDown(false)
+
+      lastScrollTop = currentScrollTop;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    console.log(isScrollDown);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isScrollDown]);
 
   return (
-    <nav className={`${styles.navBar} ${isMenuClicked && styles.clicked}`}>
+    <nav className={`${styles.navBar} ${isMenuClicked && styles.clicked} ${isScrollDown && styles.scrollDown}`}>
       <div>
-        <Image 
+        <Image
           src="/logo.png"
           alt=""
           height={50}
           width={50}
           quality={100}
-          unoptimized = {true}
+          unoptimized={true}
         />
         <button onClick={() => setIsMenuClicked(!isMenuClicked)}>
           {isMenuClicked ? (
